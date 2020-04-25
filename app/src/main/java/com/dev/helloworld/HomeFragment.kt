f0967a2de3cc3fab64550dfdec42c67e6e722eab
june <brainyjun@gmail.com>
2020-04-25 16:34:14 +0900

    sopt 2nd seminar work

---
+++ app/src/main/java/com/dev/helloworld/HomeFragment.kt
@@ -0,0 +1,45 @@
+package com.dev.helloworld
+
+import android.graphics.Rect
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        rvSomething.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = HomeRecyclerViewAdapter().apply { notifyDataSetChanged() }
+            addItemDecoration(object: RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    super.getItemOffsets(outRect, view, parent, state)
+                    if (parent.getChildAdapterPosition(view) != (parent.adapter?.itemCount?.minus(1)))
+                        outRect.bottom = 10
+                }
+            })
+        }
+    }
+}
--- app/src/main/java/com/dev/helloworld/MainActivity.kt
+++ app/src/main/java/com/dev/helloworld/MainActivity.kt
@@ -2,11 +2,63 @@ package com.dev.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragments = listOf(HomeFragment(), BookFragment(), PersonFragment())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewPager()
+        interlockViewPagerAndNavigation()
+    }
+
+    private fun initViewPager() {
+        viewPager.apply {
+            adapter = ViewPagerAdapter()
+            offscreenPageLimit = fragments.size
+        }
+    }
+
+    private fun interlockViewPagerAndNavigation() {
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuHome -> viewPager.currentItem = 0
+                R.id.menuBook -> viewPager.currentItem = 1
+                R.id.menuPerson -> viewPager.currentItem = 2
+            }
+            true
+        }
+
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                bottomNav.menu.getItem(position).isChecked = true
+            }
+
+        })
+    }
+
+    inner class ViewPagerAdapter : FragmentPagerAdapter(supportFragmentManager) {
+
+        override fun getItem(position: Int): Fragment = fragments[position]
+
+        override fun getCount(): Int = fragments.size
     }
 }
+
