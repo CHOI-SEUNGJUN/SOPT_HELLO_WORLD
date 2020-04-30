615903fd132761b7c1ff5d1eac7d33f2ddc3b9d2
june <brainyjun@gmail.com>
2020-04-30 14:38:04 +0900

    something...

---
+++ app/src/main/java/com/dev/helloworld/SignUpActivity.kt
@@ -0,0 +1,27 @@
+package com.dev.helloworld
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        initButton()
+    }
+
+    private fun initButton() {
+        btnSignUp.setOnClickListener {
+            val intent = Intent().apply {
+                putExtra("id", edtId.text.toString())
+                putExtra("pw", edtPassword.text.toString())
+            }
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+}
